672a5b50f792780ec608b0f4f9465f14347d4eab
Matias Daloia <matidaloia@gmail.com>
2023-06-08 13:30:15 +0200

    Initialized fuulfactory subgraph

---
+++ src/mappings/fuul-project.ts
@@ -0,0 +1,49 @@
+import { Address } from "@graphprotocol/graph-ts";
+import { Budget } from "../../generated/schema";
+import {
+  FungibleBudgetDeposited as FungibleBudgetDepositedEvent,
+  Attributed as AttributedEvent,
+} from "../../generated/templates/FuulProject/FuulProject";
+
+export function handleFungibleBudgetDeposited(
+  event: FungibleBudgetDepositedEvent
+): void {
+  let entity = new Budget(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+
+  entity.projectContractAddress = event.params.account; // Address of the project contract where the budget was deposited
+  entity.amount = event.params.amount; // Amount of the budget deposited
+  entity.currency = event.params.currency; // Address of the currency contract
+  entity.ownerAddress = event.params.account; // In this case the owner is the same as the project contract address
+
+  entity.blockNumber = event.params._event.block.number;
+  entity.blockTimestamp = event.params._event.block.timestamp;
+
+  entity.save();
+}
+
+export function handleAttributed(event: AttributedEvent): void {
+  const receivers = event.params.receivers;
+  const amounts = event.params.amounts;
+  const currency = event.params.currency;
+  const projectContractAddress = event.params._event.transaction.from;
+
+  for (let index = 0; index < receivers.length; index++) {
+    const receiverAddress = receivers[index];
+
+    let entity = new Budget(
+      event.transaction.hash.concatI32(event.logIndex.toI32())
+    );
+
+    entity.projectContractAddress = projectContractAddress;
+    entity.amount = amounts[index];
+    entity.currency = currency;
+    entity.ownerAddress = receiverAddress;
+
+    entity.blockNumber = event.params._event.block.number;
+    entity.blockTimestamp = event.params._event.block.timestamp;
+
+    entity.save();
+  }
+}
--- tests/fuul-factory.test.ts
+++ tests/fuul-factory.test.ts
@@ -4,33 +4,33 @@ import {
   test,
   clearStore,
   beforeAll,
-  afterAll
-} from "matchstick-as/assembly/index"
-import { BigInt, Address } from "@graphprotocol/graph-ts"
-import { AttributorFeeUpdated } from "../generated/schema"
-import { AttributorFeeUpdated as AttributorFeeUpdatedEvent } from "../generated/FuulFactory/FuulFactory"
-import { handleAttributorFeeUpdated } from "../src/fuul-factory"
-import { createAttributorFeeUpdatedEvent } from "./fuul-factory-utils"
+  afterAll,
+} from "matchstick-as/assembly/index";
+import { BigInt, Address } from "@graphprotocol/graph-ts";
+import { AttributorFeeUpdated } from "../generated/schema";
+import { AttributorFeeUpdated as AttributorFeeUpdatedEvent } from "../generated/FuulFactory/FuulFactory";
+import { handleAttributorFeeUpdated } from "../src/mappings/fuul-factory";
+import { createAttributorFeeUpdatedEvent } from "./fuul-factory-utils";
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
 
 describe("Describe entity assertions", () => {
   beforeAll(() => {
-    let value = BigInt.fromI32(234)
-    let newAttributorFeeUpdatedEvent = createAttributorFeeUpdatedEvent(value)
-    handleAttributorFeeUpdated(newAttributorFeeUpdatedEvent)
-  })
+    let value = BigInt.fromI32(234);
+    let newAttributorFeeUpdatedEvent = createAttributorFeeUpdatedEvent(value);
+    handleAttributorFeeUpdated(newAttributorFeeUpdatedEvent);
+  });
 
   afterAll(() => {
-    clearStore()
-  })
+    clearStore();
+  });
 
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
   test("AttributorFeeUpdated created and stored", () => {
-    assert.entityCount("AttributorFeeUpdated", 1)
+    assert.entityCount("AttributorFeeUpdated", 1);
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
@@ -38,9 +38,9 @@ describe("Describe entity assertions", () => {
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "value",
       "234"
-    )
+    );
 
     // More assert options:
     // https://thegraph.com/docs/en/developer/matchstick/#asserts
-  })
-})
+  });
+});
